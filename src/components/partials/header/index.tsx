55ea9faac0e82f33c2d1e0390ee7e96bf2bc5889
MohammadReza  Sadati <sadaty05@gmail.com>
2024-08-24 18:04:20 +0330

    add theme switcher

--- src/components/partials/header/index.tsx
+++ src/components/partials/header/index.tsx
@@ -1,5 +1,11 @@
+import ThemeSwitcher from "@/components/common/theme-switcher";
+
 const Header = () => {
-  return <header className="sticky h-14 w-full shadow-sm">header</header>;
+  return (
+    <header className="sticky flex h-14 w-full items-center justify-between shadow-sm">
+      <ThemeSwitcher />
+    </header>
+  );
 };
 
 export default Header;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode:'class',
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -13,6 +14,21 @@ const config: Config = {
           DEFAULT: "#4361ee",
           light: "#eaf1ff",
           "dark-light": "rgba(67,97,238,.15)",
+        },
+        dark: {
+            DEFAULT: '#3b3f5c',
+            light: '#eaeaec',
+            'dark-light': 'rgba(59,63,92,.15)',
+        },
+        black: {
+            DEFAULT: '#0e1726',
+            light: '#e3e4eb',
+            'dark-light': 'rgba(14,23,38,.15)',
+        },
+        white: {
+            DEFAULT: '#ffffff',
+            light: '#e0e6ed',
+            dark: '#888ea8',
         },
       },
     },
