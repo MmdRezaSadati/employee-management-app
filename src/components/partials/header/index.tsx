d9454c45a9ed9258b3b2bcb48d735cf23cb44374
MohammadReza  Sadati <sadaty05@gmail.com>
2024-08-24 16:22:25 +0330

    separation components

---
+++ src/components/partials/header/index.tsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <header className="sticky h-14 w-full shadow-sm">header</header>;
+};
+
+export default Header;
---
+++ src/components/partials/sidebar/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div className="fixed top-0 z-50 h-screen w-64 shadow-[5px_0_25px_0_rgba(94,92,154,0.1)] ltr:left-0 rtl:right-0">
+      Sidebar
+    </div>
+  );
+};
+
+export default Sidebar;
