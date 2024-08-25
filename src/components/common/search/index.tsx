c6a471cf197abdcebf4f5b7e4e1f2537cf3f7276
MohammadReza  Sadati <sadaty05@gmail.com>
2024-08-25 03:47:37 +0330

    update search component conditions

--- src/components/common/search/index.tsx
+++ src/components/common/search/index.tsx
@@ -1,10 +1,7 @@
 "use client";
 import IconSearch from "@/core/icons/icon-search";
 import IconXCircle from "@/core/icons/icon-x-circle";
-import {
-  onSearchChange,
-  TSearchSelector,
-} from "@/stores/slices/search";
+import { onSearchChange, TSearchSelector } from "@/stores/slices/search";
 import { useDispatch, useSelector } from "react-redux";
 
 const Search = () => {
@@ -23,7 +20,7 @@ const Search = () => {
         <div className="relative">
           <input
             type="text"
-            className=" peer  rounded-md border w-32 sm:w-auto border-white-light bg-gray-100 px-4 py-2 text-sm font-semibold text-black !outline-none placeholder:tracking-widest focus:border-primary focus:ring-transparent dark:border-[#17263c] dark:bg-[#121e32] dark:text-white-dark dark:focus:border-primary sm:bg-transparent ltr:px-9 ltr:sm:pr-4 rtl:px-9 rtl:sm:pl-4"
+            className=" peer  w-32 rounded-md border border-white-light bg-gray-100 px-4 py-2 text-sm font-semibold text-black !outline-none placeholder:tracking-widest focus:border-primary focus:ring-transparent dark:border-[#17263c] dark:bg-[#121e32] dark:text-white-dark dark:focus:border-primary sm:w-auto sm:bg-transparent ltr:px-9 ltr:sm:pr-4 rtl:px-9 rtl:sm:pl-4"
             placeholder="Search..."
             onChange={(v) => setSearch(v.target.value)}
           />
@@ -32,7 +29,7 @@ const Search = () => {
             className="absolute inset-0 size-9 appearance-none peer-focus:text-primary ltr:right-auto rtl:left-auto">
             <IconSearch className="mx-auto" />
           </button>
-          {searchValue && (
+          {searchValue !== false && searchValue !== "" && (
             <button
               type="button"
               className="absolute top-1/2 block -translate-y-1/2 hover:opacity-80 sm:hidden ltr:right-2 rtl:left-2"
