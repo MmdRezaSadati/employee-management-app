7c9fabdbd726dd6b2a3a20aec1afaf3f0a080202
MohammadReza  Sadati <sadaty05@gmail.com>
2024-08-27 01:26:03 +0330

    add get employee by id in profile and dynamic page

--- src/app/(panel-routes)/profile/[id]/page.tsx
+++ src/app/(panel-routes)/profile/[id]/page.tsx
@@ -1,9 +1,23 @@
+import Hydrate from "@/components/common/hydrate";
 import Profile from "@/components/pages/profile";
+import { EMPLOYEE } from "@/core/constants/api.constants";
+import { getEmployeeById } from "@/core/services/api/employee.api";
 import { TFCParamsId } from "@/core/types";
+import { QueryClient, dehydrate } from "@tanstack/react-query";
 import React from "react";
 
-const ProfilePage: TFCParamsId = ({ params: { id } }) => {
-  return <Profile id={id} />;
+const ProfilePage: TFCParamsId = async ({ params: { id } }) => {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: [EMPLOYEE, "edit"],
+    queryFn: () => getEmployeeById({ data: { Id: id } }),
+  });
+  return (
+    <Hydrate state={dehydrate(queryClient)}>
+      <Profile id={id} />
+    </Hydrate>
+  );
 };
 
 export default ProfilePage;
