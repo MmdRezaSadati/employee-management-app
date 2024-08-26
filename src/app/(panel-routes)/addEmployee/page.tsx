8e5d1bccbd976209ef2422c9027dd450aabefb30
MohammadReza  Sadati <sadaty05@gmail.com>
2024-08-26 04:24:14 +0330

    add addEmployee page

---
+++ src/app/(panel-routes)/addEmployee/page.tsx
@@ -0,0 +1,8 @@
+import AddEmployee from "@/components/pages/addEmployee";
+import React from "react";
+
+const AddEmployeePage = () => {
+  return <AddEmployee />;
+};
+
+export default AddEmployeePage;
