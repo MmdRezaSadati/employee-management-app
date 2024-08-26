e262d1d77fc2c2b7f77c5db6ddaee40f2d7e1001
MohammadReza  Sadati <sadaty05@gmail.com>
2024-08-26 17:01:08 +0330

    add react query and config for SSR

--- src/components/provider/index.tsx
+++ src/components/provider/index.tsx
@@ -2,14 +2,19 @@
 import { TFCChildren } from "@/core/types";
 import store from "@/stores";
 import { NextUIProvider } from "@nextui-org/react";
-import React from "react";
 import { Provider as ReduxProvider } from "react-redux";
 
+import getQueryClient from "@/core/utils/query-client.utils";
+import { QueryClientProvider } from "@tanstack/react-query";
+
 const MainProvider: TFCChildren = ({ children }) => {
+  const queryClient = getQueryClient();
   return (
-    <ReduxProvider store={store}>
-      <NextUIProvider>{children}</NextUIProvider>
-    </ReduxProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReduxProvider store={store}>
+        <NextUIProvider>{children}</NextUIProvider>
+      </ReduxProvider>
+    </QueryClientProvider>
   );
 };
 
