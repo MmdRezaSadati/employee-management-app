587e6094d569ccc2e458d80d97d45600c1990a60
MohammadReza  Sadati <sadaty05@gmail.com>
2024-08-27 23:49:16 +0330

    error handling with try and catch

--- src/core/services/api/employee.api.ts
+++ src/core/services/api/employee.api.ts
@@ -2,13 +2,18 @@ import { IUser } from "@/components/common/employee-table";
 import { IIdParams, IRequestBody } from "@/core/types/common/api.interface";
 import axiosInterceptorInstance from "../axios/interceptor/interceptor.axios";
 import { EMPLOYEE } from "@/core/constants/api.constants";
+import { toast } from "react-toastify";
 
 export const addEmployee = async ({
   data,
-}: IRequestBody<IUser>): Promise<IUser> => {
-  return await axiosInterceptorInstance
-    .post(EMPLOYEE, data)
-    .then((res) => res.data);
+}: IRequestBody<IUser>): Promise<IUser | any> => {
+  try {
+    return await axiosInterceptorInstance
+      .post(EMPLOYEE, data)
+      .then((res) => res.data);
+  } catch (error) {
+    toast.error("An unknown problem occurred");
+  }
 };
 
 export const getEmployees: () => Promise<IUser[]> = async () => {
@@ -16,24 +21,36 @@ export const getEmployees: () => Promise<IUser[]> = async () => {
 };
 export const getEmployeeById: (
   prop: IRequestBody<IIdParams>
-) => Promise<IUser> = async ({ data }) => {
-  return await axiosInterceptorInstance
-    .get(EMPLOYEE + `/${data.Id}`)
-    .then((res) => res.data);
+) => Promise<IUser | any> = async ({ data }) => {
+  try {
+    return await axiosInterceptorInstance
+      .get(EMPLOYEE + `/${data.Id}`)
+      .then((res) => res.data);
+  } catch (error) {
+    toast.error("An unknown problem occurred");
+  }
 };
 
 export const deleteEmployee = async ({
   data,
-}: IRequestBody<IIdParams>): Promise<IUser> => {
-  return await axiosInterceptorInstance
-    .delete(EMPLOYEE + `/${data.Id}`)
-    .then((res) => res.data);
+}: IRequestBody<IIdParams>): Promise<IUser | any> => {
+  try {
+    return await axiosInterceptorInstance
+      .delete(EMPLOYEE + `/${data.Id}`)
+      .then((res) => res.data);
+  } catch (error) {
+    toast.error("An unknown problem occurred");
+  }
 };
 
 export const editEmployee = async ({
   data,
-}: IRequestBody<IUser>): Promise<IUser> => {
-  return await axiosInterceptorInstance
-    .put(EMPLOYEE + `/${data.id}`, data)
-    .then((res) => res.data);
+}: IRequestBody<IUser>): Promise<IUser | any> => {
+  try {
+    return await axiosInterceptorInstance
+      .put(EMPLOYEE + `/${data.id}`, data)
+      .then((res) => res.data);
+  } catch (error) {
+    toast.error("An unknown problem occurred");
+  }
 };
